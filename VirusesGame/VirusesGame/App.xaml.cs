3de37a16b4321bce35b108471ca3737d184822e5
Kamil <kamilgilfanov6@gmail.com>
2024-06-30 17:17:01 +0300

    create app

---
+++ VirusesGame/VirusesGame/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VirusesGame
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VirusesGame/VirusesGame/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VirusesGame
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
